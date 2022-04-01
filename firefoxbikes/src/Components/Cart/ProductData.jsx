56dd18ba0b394800e74e6c4ef8624d2d875087a8
Rana Sarkar <rana.sarkarg3@gmail.com>
2022-04-01 11:57:37 +0530

    delete and wishlist icon added

--- firefoxbikes/src/Components/Cart/ProductData.jsx
+++ firefoxbikes/src/Components/Cart/ProductData.jsx
@@ -1,3 +1,6 @@
+import heartIcon from "./Images/heart.png";
+import deleteIcon from "./Images/delete.png";
+
 export const ProductData = ({ _id, title, price, color, image, section }) => {
   return (
     <>
@@ -10,26 +13,49 @@ export const ProductData = ({ _id, title, price, color, image, section }) => {
             borderRadius: "5px",
           }}
         >
-          <img src={image} alt="" style={{ width: "170px", height: "150px"}} />
+          <img src={image} alt="" style={{ width: "170px", height: "150px" }} />
         </div>
 
         {/* Product Data */}
-        <div style={{ marginLeft: "30px" }}>
-          <h3>
-            {title} {section}
-          </h3>
-          <div
-            style={{
-              width: "20px",
-              height: "20px",
-              borderRadius: "50%",
-              backgroundColor: `${color[0]}`,
-            }}
-          ></div>
-        </div>
-        <div style={{ marginLeft: "50%" }}>
-          <p style={{fontWeight: "bold"}}>Price</p>
-          <h4>₹{price}</h4>
+        <div className="product-data">
+          
+          <div className="name-price" style={{display: "flex"}} >
+          <div style={{ marginLeft: "30px"}}>
+            <h3>
+              {title} {section}
+            </h3>
+            <div
+              style={{
+                width: "20px",
+                height: "20px",
+                borderRadius: "50%",
+                backgroundColor: `${color[0]}`,
+              }}
+            ></div>
+          </div>
+          <div style={{ marginLeft: "50%" }}>
+            <p style={{ fontWeight: "bold" }}>Price</p>
+            <h4>₹{price}</h4>
+          </div>
+          </div>
+
+          {/* DELETE Button and Wishlist Button */}
+          <div className="delete" style={{ display: "flex", justifyContent: "space-around" }}>
+            <button style={{border: "none", backgroundColor: "transparent",}} >
+              <img
+                src={heartIcon}
+                alt=""
+                style={{ width: "25px", height: "30px" }}
+              />
+            </button>
+            <button style={{border: "none", backgroundColor: "transparent",}} >
+              <img
+                src={deleteIcon}
+                alt=""
+                style={{ width: "25px", height: "30px" }}
+              />
+            </button>
+          </div>
         </div>
       </div>
     </>
