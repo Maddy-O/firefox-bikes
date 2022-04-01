73d3424ed74a7c98d352ec24a3ab2a7213140afe
Maddy <95930594+Maddy-O@users.noreply.github.com>
2022-04-01 10:25:36 +0530

    Update

--- firefoxbikes/src/Components/BikeService/BikeService.jsx
+++ firefoxbikes/src/Components/BikeService/BikeService.jsx
@@ -1,47 +1,51 @@
 import { ServiceStationForm } from "../BikeService/ServiceStationForm";
 import { HomeServiceForm } from "../BikeService/HomeServiceForm";
 import { useState } from "react";
-import ServicePackages from "./ServicePackages";
+import styled from "styled-components";
+
 
 export const BikeService = () => {
   const [isToggled, setIsToggled] = useState(true);
   return (
-    <div style={{ marginTop: "25px" }}>
-      <button
-        onClick={() => {
-          setIsToggled(!isToggled);
-        }}
-        style={{
-          backgroundColor: "black",
-          color: "white",
-          height: "50px",
-          width: "200px",
-          border: "0px",
-          margin: "15px",
-          fontWeight: "bolder",
-        }}
-      >
-        SERVICE STATION
-      </button>
-      <button
-        onClick={() => {
-          setIsToggled(!isToggled);
-        }}
-        style={{
-          backgroundColor: "#ff5000",
-          color: "white",
-          height: "50px",
-          width: "200px",
-          border: "0px",
-          margin: "15px",
-          fontWeight: "bolder",
-        }}
-      >
-        HOME SERVICE
-      </button>
-      {isToggled ? <ServiceStationForm /> : <HomeServiceForm />}
-      {/* <ServicePackages /> */}
-    </div>
+    <>
+      <img src="bikes-banner_7.jpg"></img>
+      <div style={{ marginTop: "25px" }}>
+        <button
+          onClick={() => {
+            setIsToggled(!isToggled);
+          }}
+          style={{
+            backgroundColor: "black",
+            color: "white",
+            height: "50px",
+            width: "200px",
+            border: "0px",
+            margin: "15px",
+            fontWeight: "bolder",
+          }}
+        >
+          SERVICE STATION
+        </button>
+        <button
+          onClick={() => {
+            setIsToggled(!isToggled);
+          }}
+          style={{
+            backgroundColor: "#ff5000",
+            color: "white",
+            height: "50px",
+            width: "200px",
+            border: "0px",
+            margin: "15px",
+            fontWeight: "bolder",
+          }}
+        >
+          HOME SERVICE
+        </button>
+        {isToggled ? <ServiceStationForm /> : <HomeServiceForm />}
+        {/* <ServicePackages /> */}
+      </div>
+    </>
   );
 };
 //-----------------------Conditional rendring-----------------/
--- firefoxbikes/src/Components/Routers/Routers.jsx
+++ firefoxbikes/src/Components/Routers/Routers.jsx
@@ -3,17 +3,18 @@ import { BikeService } from "../BikeService/BikeService";
 import { Route, Routes } from "react-router-dom";
 import { ProductPage } from "../ProductPage/ProductPage";
 import { Cart } from "../Cart/Cart";
+import Home from "../Home/Home";
 
 export const Routers = () => {
   return (
     <>
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/product" element={<ProductPage />} />
-            <Route path="/home/" element={<BikeService />} />
-          <Route path="/cart" element={<Cart />}/>
-        </Routes>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/product" element={<ProductPage />} />
+        <Route path="/bikeservice" element={<BikeService />} />
+        <Route path="/cart" element={<Cart />} />
+      </Routes>
     </>
   );
 };
