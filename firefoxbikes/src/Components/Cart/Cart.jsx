31be23d66c1f3c41b39d6071657a84fa6b990102
Amaresh Barik <96060638+amareshcoding@users.noreply.github.com>
2022-04-02 11:27:07 +0530

    cart total price

--- firefoxbikes/src/Components/Cart/Cart.jsx
+++ firefoxbikes/src/Components/Cart/Cart.jsx
@@ -1,7 +1,13 @@
+import { useState } from "react";
 import { PriceDetails } from "./PriceDetails";
 import { ProductInfo } from "./ProductInfo";
 
 export const Cart = () => {
+  const [cart_total, setCart_total] = useState(0)
+  const totalPrice=(total)=>{
+    setCart_total(total)
+    // console.log("total in cart", total)
+  }
   return (
     <>
       <div
@@ -16,13 +22,15 @@ export const Cart = () => {
         <div style={{ color: "#3f4351" }}>SHOPPING</div>
         <div style={{ color: "silver" }}>CART</div>
       </div>
+
       <div
         className="Cart-main"
         style={{ display: "flex", margin: "10%" ,  gap: "5%" }}
       >
-        <ProductInfo />
+        <ProductInfo totalPrice={totalPrice}/>
+
         <div className="right-container">
-          <PriceDetails />
+          <PriceDetails cart_total={cart_total} />
         </div>
       </div>
     </>
