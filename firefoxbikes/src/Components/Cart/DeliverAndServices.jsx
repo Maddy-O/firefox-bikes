a3b02b3b4aa90f8326b4b5fcc90d8cf9e07eef59
Rana Sarkar <rana.sarkarg3@gmail.com>
2022-04-03 11:15:03 +0530

    Pincode status

--- firefoxbikes/src/Components/Cart/DeliverAndServices.jsx
+++ firefoxbikes/src/Components/Cart/DeliverAndServices.jsx
@@ -1,6 +1,22 @@
+import axios from "axios";
+import { useState } from "react";
+
 export const DeliverServices = () => {
+  const [pin, setPin] = useState("");
+  const [pinData, setPinData] = useState([]);
+  
+  const pinCode = () => {
+    axios.get(`https://api.postalpincode.in/pincode/${pin}`).then((res) => {
+      let PostOffice = res.data[0].PostOffice[1];
+      setPinData(PostOffice);
+      // console.log(PostOffice);
+      console.log(pinData);
+    })
+  };
+  
   return (
     <>
+      <h5>{pinData.state}</h5>
       <div className="delivery" style={{ margin: "5%", marginLeft: "10%" }}>
         <div style={{ fontSize: "30px", marginLeft: "20px" }}>DELIVERY &</div>
         <div
@@ -13,26 +29,44 @@ export const DeliverServices = () => {
           style={{
             display: "flex",
             width: "60%",
-            height: "180px",
+            height: "auto",
             margin: "2%",
             marginLeft: "2%",
             backgroundColor: "whitesmoke",
           }}
         >
           <div style={{ margin: "20px", padding: "5px" }}>
-            
-            <div style={{width:'250px', borderRadius: "5px", display: 'flex',
-                  border: "1px solid grey", height: "30px", padding: "5px",}} >
+            <div
+              style={{
+                width: "250px",
+                borderRadius: "5px",
+                display: "flex",
+                border: "1px solid grey",
+                height: "30px",
+                padding: "5px",
+              }}
+            >
               <input
                 type="number"
                 placeholder="Enter Pincode"
                 style={{
-                  border:'none',
-                  textAlign: 'left',
-                  backgroundColor: 'transparent',
+                  border: "none",
+                  textAlign: "left",
+                  backgroundColor: "transparent",
                 }}
-              / >
-              <button style={{border:'none', cursor:'pointer', backgroundColor: 'transparent', color:'tomato'}} >Check</button>
+                onChange={(e) => setPin(e.target.value)}
+              />
+              <button
+                style={{
+                  border: "none",
+                  cursor: "pointer",
+                  backgroundColor: "transparent",
+                  color: "tomato",
+                }}
+                onClick={pinCode}
+              >
+                Check
+              </button>
             </div>
             <div
               style={{
@@ -43,6 +77,13 @@ export const DeliverServices = () => {
             >
               Free Home Delivery
             </div>
+
+            
+            <div className="pincodeData">
+              {/* <p>{pinData.State}</p> */}
+              
+            </div>
+
             <div>The product will be delivered within 10-12 days</div>
           </div>
           <div style={{ margin: "15px", padding: "5px" }}>
